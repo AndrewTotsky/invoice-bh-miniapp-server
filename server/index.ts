a9e53ea9f06ade7ff52ed2f8a48267478a0bcb39
AndrewTotsky <andrewtotsky@skbkontur.ru>
2025-09-22 23:40:26 +0700

    cors

--- server/index.ts
+++ server/index.ts
@@ -24,7 +24,13 @@ const upload = multer({
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://AndrewTotsky.github.io'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, '../dist')));
 
